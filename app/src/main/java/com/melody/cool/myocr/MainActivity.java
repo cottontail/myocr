12cd4f0c4fea6d91e03a4bf8987a59fc8c52913f
haobo yu <haoboyu@gmail.com>
2015-10-11 08:32:51 -0700

    first commit

---
+++ app/src/main/java/com/melody/cool/myocr/MainActivity.java
@@ -0,0 +1,158 @@
+package com.melody.cool.myocr;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_GALLERY = 0;
+    private static final int REQUEST_CAMERA = 1;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private TessBaseAPI baseAPI;
+    private Uri imageUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        baseAPI = new TessBaseAPI();
+        File externalStorageDirectory = Environment.getExternalStorageDirectory();
+
+        File appDir = new File(externalStorageDirectory, "ocrsample");
+        if (!appDir.isDirectory())
+            appDir.mkdir();
+
+        final File baseDir = new File(appDir, "tessdata");
+        if (!baseDir.isDirectory())
+            baseDir.mkdir();
+
+        baseAPI.init(appDir.getPath(), "eng");
+
+        findViewById(R.id.choose_from_gallery).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, REQUEST_GALLERY);
+            }
+        });
+
+        findViewById(R.id.take_a_photo).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String filename = System.currentTimeMillis() + ".jpg";
+
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Images.Media.TITLE, filename);
+                values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+                imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+                Intent intent = new Intent();
+                intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(intent, REQUEST_CAMERA);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_GALLERY:
+                if (resultCode == RESULT_OK) {
+                    inspect(data.getData());
+                }
+                break;
+            case REQUEST_CAMERA:
+                if (resultCode == RESULT_OK) {
+                    if (imageUri != null) {
+                        inspect(imageUri);
+                    }
+                }
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+                break;
+        }
+    }
+
+    private void inspectFromBitmap(Bitmap bitmap) {
+        //baseAPI.setPageSegMode(TessBaseAPI.AVS_MOST_ACCURATE);
+        //baseAPI.setPageSegMode(TessBaseAPI.PSM_SINGLE_LINE);
+        baseAPI.setPageSegMode(TessBaseAPI.PageSegMode.PSM_AUTO);
+        baseAPI.setImage(bitmap);
+        String text = baseAPI.getUTF8Text();
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+        bitmap.recycle();
+    }
+
+    private void inspect(Uri uri) {
+        InputStream is = null;
+        try {
+            is = getContentResolver().openInputStream(uri);
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+            options.inSampleSize = 2;
+            options.inScreenDensity = DisplayMetrics.DENSITY_LOW;
+            Bitmap bitmap = BitmapFactory.decodeStream(is, null, options);
+            inspectFromBitmap(bitmap);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
